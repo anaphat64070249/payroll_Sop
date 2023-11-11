c7e15627d176c2aade522e2a5b5ec958a85d5156
Anaphat Nuntanapiboon <88420410+anaphat64070249@users.noreply.github.com>
2023-11-12 01:38:49 +0700

    1.38

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ app.use(express.static('static'))
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-const leave = require('./salay_all/salary')
-const update = require('./salarybase/salarybase')
+const all = require("./salay_all/salary")
+const base = require("./salarybase/salarybase")
+const deduc = require("./deduction/deduction")
+const add = require("./addition/addition") 
 
 app.use(cors())
-app.use(leave.router)
-app.use(update.router)
+app.use(all.router)
+app.use(base.router)
+app.use(deduc.router)
+app.use(add.router)
 
 app.listen(3000, () => {})
