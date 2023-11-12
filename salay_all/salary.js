c2658a28178703d4bd5e0baeac0829850bdeaa55
Anaphat Nuntanapiboon <88420410+anaphat64070249@users.noreply.github.com>
2023-11-12 15:05:08 +0700

    15.05

--- salay_all/salary.js
+++ salay_all/salary.js
@@ -8,10 +8,10 @@ router.get("/payroll_all", async (req,res,next) => {
     try{
 
 
-    const [sum,fields] = await pool.query("select sum(position_salary) as sum_salary,sum(addition_amount) as sum_add,sum(netSalary) as net_salary,sum(position_salary)+sum_salary,sum(addition_amount)-sum_add,sum(netSalary) as sum_deduction from Payroll join Employee_info using(emp_id) join Position using(position_id) join Addition using(addition_id)")
+    const [sum,fields] = await pool.query("select sum(position_salary) as sum_salary,sum(addition_amount)+(count(Payroll.emp_id)*(netSalary+ (netSalary*percent/100))) as sum_add,sum(netSalary) as net_salary,sum(position_salary)+sum(addition_amount)+(count(Payroll.emp_id)*(netSalary+ (netSalary*percent/100)))-sum(netSalary) as sum_deduction from Payroll join Employee_info using(emp_id) join Position using(position_id) join Addition using(addition_id)")
         
     
-    const [everyone,fields2] = await pool.query("select * from Payroll join Employee_info using(emp_id) join Position using(position_id) ")
+    const [everyone,fields2] = await pool.query("select * from Payroll join Employee_info using(emp_id) join Position using(position_id) join Addition using (addition_id)")
     
     res.json({sum:sum[0],everyone:everyone})
     
